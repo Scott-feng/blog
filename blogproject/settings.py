4a746cc93b9f889c84976ee101670c1dec91e527
scott-feng <tryqtyl2009@qq.com>
2017-12-10 09:54:41 +0800

    deploy change

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'myc82*m+@a73e2ahzk8wz=*dn+03u1l_zn#+6i$gxov7gp56!4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.scottshen.link']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
